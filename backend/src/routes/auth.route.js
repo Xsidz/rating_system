af3c2715da4d3c55980d70b178751f016d9e30d1
Xsidz <siddhesh.kabraa@gmail.com>
2025-08-14 23:15:56 +0530

    login

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,9 @@
-import express from "express"
+import express from "express";
 import { requireAuth } from "../middlewares/auth.middleware.js";
-import { signup } from "../controllers/auth.controllers.js";
+import { login, signup } from "../controllers/auth.controllers.js";
 const router = express.Router();
 
-router.post("/signup",signup)
-
-
-
+router.post("/signup", signup);
+router.post("/login", login);
 
 export default router;
